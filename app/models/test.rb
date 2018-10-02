cbd22f0513d4db921ca77e2138416dc053558c20
Daria Shcheglova <ellerden@gmail.com>
2018-10-02 18:19:36 +0300

    do queries - user passed test by level, tests by categories

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.by_category(category)
+    cat = Category.find_by(title: category)
+    Test.where(category_id: cat.id).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def test_passed_by_level(level)
+    test = Test.find_by_level(level)
+    passed = Participation.where(user_id: id).find_by(test_id: test.id)
+    Test.where(id: passed.test_id)
+  end
 end
