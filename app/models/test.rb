844ff5ef13354459153402b643a211047aa5ddc8
Daria Shcheglova <ellerden@gmail.com>
2018-10-07 17:40:15 +0200

    fix strange missing associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :users, through: :participations
+  has_many :questions
+  belongs_to :category
+#????
+  belongs_to :author, class_name: 'User'
+
   def self.by_category(category)
     Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
         .where(categories: { title: category })
