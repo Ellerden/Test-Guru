7b59b1258294eb7dd1bee9157a8ba253e6ff0199
Daria Shcheglova <ellerden@gmail.com>
2018-10-09 22:47:08 +0200

    change logic of validating amount of answers question has, add translation of active model errors to locale

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,11 @@ class Answer < ApplicationRecord
   belongs_to :question
   scope :right, -> { where(correct: true) }
   validates :text, presence: true
+  validate :answers_length
+
+  def answers_length
+    return if question.answers.length <= 4
+
+    error.add(:answers, 'There should be between 1 to 4 answers')
+  end
 end
