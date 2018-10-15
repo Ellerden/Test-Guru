cd5bb2abc52ca32165aa4d9257c78dee3431c6c3
Daria Shcheglova <ellerden@gmail.com>
2018-10-15 20:30:07 +0300

    create question create and question destroy methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,36 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, except: [:show, :create]
+  before_action :find_question, only: %i[pluck destroy]
 
   # /tests/:test_id/questions/new(.:format)
   def new
+    @question = Question.new(test: @test)
+  end
+
+  def create
+
+    #result = ["class: #{params.class}, params: #{params.inspect}"]
+   # render plain: result.join("\n")
+    @new_question = Question.create(question_params)
+    render plain: @new_question.inspect
   end
 
    #result = ["class: #{params.class}, params: #{params.inspect}"]
    # render plain: result.join("\n")
 
   def destroy
+    @question.destroy
+    redirect_to index
   end
 
   # /questions/:id(.:format)
   def show
     render plain: @question.text
+  end
+
+  def pluck
+    result = []
+    @question.map { |q| render plain: q.text }
   end
 
  # /tests/:test_id/questions
@@ -30,5 +47,13 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:text, :test_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'tests#index'
+
   resources :tests do
     resources :questions, shallow: true
   end
+  get '/tests/:test_id/questions/:id(.:format)' =>'test_questions#pluck'
+  post '/questions' =>'questions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
