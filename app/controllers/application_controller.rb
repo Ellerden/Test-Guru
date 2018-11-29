66115b4c44a87a18084921d87197767fa99440ab
Daria Shcheglova <ellerden@gmail.com>
2018-11-29 15:00:04 +0300

    add flash welcome message after log in with first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Привет, #{current_user.first_name} #{current_user.last_name}!"
     if current_user.is_a?(Admin)
       admin_tests_path
     else
       stored_location_for(resource) || root_path
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
   end
 end
