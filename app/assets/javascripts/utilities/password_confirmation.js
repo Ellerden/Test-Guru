760a87bb405429792512135e3af2d0b548547fec
Daria Shcheglova <ellerden@gmail.com>
2018-12-17 14:16:18 +0300

    add semicolon to the end of js files

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -1,19 +1,19 @@
 document.addEventListener('turbolinks:load', function() {
-  var password = document.getElementById('user_password')
-  var confirmation = document.getElementById('user_password_confirmation')
+  var password = document.getElementById('user_password');
+  var confirmation = document.getElementById('user_password_confirmation');
 
-  confirmation.addEventListener("input", comparePasswords.bind(null, password, confirmation))
-  confirmation.addEventListener("change", comparePasswords.bind(null, password, confirmation))
+  confirmation.addEventListener("input", comparePasswords.bind(null, password, confirmation));
+  confirmation.addEventListener("change", comparePasswords.bind(null, password, confirmation));
 })
 
 function comparePasswords(password, confirmation) {
     if (confirmation.value == "") {
-      return 0
+      return 0;
     }
-      if (password.value == confirmation.value)
-    {
-      confirmation.className = "success"
+    if (password.value == confirmation.value);
+    {;
+      confirmation.className = "success";
     } else {
-      confirmation.className = "error"
+      confirmation.className = "error";
     }
 }
